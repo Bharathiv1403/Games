38dff8febf3326a20a381c5b2b3225d52e28316a
Bharathi V <128215224+Bharathiv1403@users.noreply.github.com>
2023-10-26 11:06:53 +0530

    Add files via upload

---
+++ bha.py
@@ -0,0 +1,32 @@
+import pygame as py
+import sys
+
+py.init()
+
+WIDTH, HEIGHT = 1200,800
+BACKGROUND_COLOR = (0, 150, 0)  # Green background
+BALL_COLOR = (300, 300, 300)    # White cricket ball
+BALL_RADIUS = 15
+
+# Create the screen
+screen = py.display.set_mode((WIDTH, HEIGHT))
+py.display.set_caption("Cricket Anime")
+
+# Main game loop
+running = True
+while running:
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            running = False
+
+    # Fill the background
+    screen.fill(BACKGROUND_COLOR)
+
+    # Draw the cricket ball
+    py.draw.circle(screen, BALL_COLOR, (WIDTH // 2, HEIGHT // 2), BALL_RADIUS)
+
+    py.display.flip()
+
+# Quit Pygame
+py.quit()
+sys.exit()
---
+++ bharathi 1.py
@@ -0,0 +1,51 @@
+import turtle as t
+
+# Set up the Turtle screen
+screen = t.Screen()
+screen.bgcolor("white")
+
+# Create a Turtle object for drawing
+ben_10 = t.Turtle()
+ben_10.speed(1)
+
+# Define a function to draw Ben 10's face
+def draw_ben_10_face():
+    ben_10.penup()
+    ben_10.goto(0, -100)
+    ben_10.pendown()
+    ben_10.begin_fill()
+    ben_10.circle(100)
+    ben_10.end_fill()
+
+# Define a function to draw Ben 10's eyes
+def draw_ben_10_eyes():
+    ben_10.penup()
+    ben_10.goto(-40, 30)
+    ben_10.pendown()
+    ben_10.begin_fill()
+    ben_10.circle(20)
+    ben_10.end_fill()
+
+    ben_10.penup()
+    ben_10.goto(40, 30)
+    ben_10.pendown()
+    ben_10.begin_fill()
+    ben_10.circle(20)
+    ben_10.end_fill()
+
+# Define a function to draw Ben 10's logo
+def draw_ben_10_logo():
+    ben_10.penup()
+    ben_10.goto(0, -10)
+    ben_10.pendown()
+    ben_10.begin_fill()
+    ben_10.circle(10)
+    ben_10.end_fill()
+
+# Draw Ben 10's face, eyes, and logo
+draw_ben_10_face()
+draw_ben_10_eyes()
+draw_ben_10_logo()
+
+# Close the Turtle graphics window on click
+screen.exitonclick()
